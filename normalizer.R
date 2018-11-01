9b180cffb05bc8fb32d9c8e2a0d25af64a28f71f
Mohammed Ali <mali@uplandsoftware.com>
2018-11-01 21:24:08 +0200

    normalize target data

--- normalizer.R
+++ normalizer.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(BBmisc)
+
 train_compound_features <- read_csv("data/train/compoundFeatures.csv")
 test_compound_features <- read_csv("data/test/compoundFeatures.csv")
 compound_features <- full_join(train_compound_features, test_compound_features)
@@ -8,3 +9,12 @@ write_csv(subset(compound_features_normalized, !is.na((compound_IDs))),
           "data/train/compoundFeaturesNormalized.csv")
 write_csv(subset(compound_features_normalized, is.na((compound_IDs))), 
           "data/test/compoundFeaturesNormalized.csv")
+
+train_target_features <- read_csv("data/train/targetFeatures.csv")
+test_target_features <- read_csv("data/test/targetFeatures.csv")
+target_features <- full_join(train_compound_features, test_target_features)
+target_features_normalized <- normalize(target_features, method = "range")
+write_csv(subset(target_features_normalized, !is.na((target_IDs))), 
+          "data/train/targetFeaturesNormalized.csv")
+write_csv(subset(target_features_normalized, is.na((target_IDs))), 
+          "data/test/targetFeaturesNormalized.csv")
