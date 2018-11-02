c4421e4c0e96847dfcc6f93b07d6469f586daaac
alizat <ali_ezzat85@yahoo.com>
2018-11-02 18:04:38 +0200

    fix feature normalization

--- normalizer.R
+++ normalizer.R
@@ -1,19 +1,55 @@
 library(tidyverse)
 library(BBmisc)
 
+
+## read compound features
 train_compound_features <- read_csv("data/train/compoundFeatures.csv")
 test_compound_features <- read_csv("data/test/compoundFeatures.csv")
-compound_features <- full_join(train_compound_features, test_compound_features)
+
+## add compound_IDs column to test data
+tmpDf <- data.frame(compound_IDs = rep(NA, nrow(test_compound_features)))
+test_compound_features <- cbind(tmpDf, test_compound_features)
+
+## combine train and test data sets together
+compound_features <- rbind(train_compound_features, test_compound_features)
+# compound_features <- full_join(train_compound_features, test_compound_features)
+
+## normalization + write to file
 compound_features_normalized <- normalize(compound_features, method = "range")
 write_csv(subset(compound_features_normalized, !is.na((compound_IDs))), 
           "data/train/compoundFeaturesNormalized.csv")
 write_csv(subset(compound_features_normalized, is.na((compound_IDs))), 
           "data/test/compoundFeaturesNormalized.csv")
 
+
+## clear environment
+rm(list = ls())
+
+
+## read target features
 train_target_features <- read_csv("data/train/targetFeatures.csv")
 test_target_features <- read_csv("data/test/targetFeatures.csv")
-target_features <- full_join(train_compound_features, test_target_features)
-target_features_normalized <- normalize(target_features, method = "range")
+
+## add target_IDs column to test data
+tmpDf <- data.frame(target_IDs = rep(NA, nrow(test_target_features)))
+test_target_features <- cbind(tmpDf, test_target_features)
+
+## combine train and test data sets together
+target_features <- rbind(train_target_features, test_target_features)
+# target_features <- full_join(train_target_features, test_target_features)
+
+## normalization
+target_features_normalized <- target_features
+target_features_normalized[,2:420]    <- 
+    target_features_normalized[,2:420]    / 100
+target_features_normalized[,691:1189] <- 
+    target_features_normalized[,691:1189] / 100
+remainingFeatures <- 1:ncol(target_features_normalized)
+remainingFeatures <- setdiff(remainingFeatures, c(2:420, 691:1189))
+target_features_normalized[,remainingFeatures] <- 
+    normalize(target_features_normalized[,remainingFeatures], method = "range")
+
+## write to file
 write_csv(subset(target_features_normalized, !is.na((target_IDs))), 
           "data/train/targetFeaturesNormalized.csv")
 write_csv(subset(target_features_normalized, is.na((target_IDs))), 
